34f0447689e75cd6c65d7d6dab112d0771581e3f
JoaoEler <jgelermendes@gmail.com>
2020-04-08 21:00:18 -0300

    frontend list, add e delete reps

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [projects, setProjects] = useState([]);
+
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() =>{
+    api.get('repositories').then(response=>{
+      setProjects(response.data);
+    }, []);
+  });
+  
+  console.log(projects)
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const form = {
+      title,
+      url,
+      techs,
+    };
+
+    const response = await api.post('repositories', form);
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id) { 
+    await api.delete(`repositories/${id}`)
   }
-
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+  {projects.map(project => <li key={project.id}>{project.title} <button key ={project.id} onClick={() => handleRemoveRepository(project.id)}>Remover</button> </li>)}
       </ul>
+
+      <input placeholder="Título"
+      value={title}
+      onChange={e => setTitle(e.target.value)}></input>
+
+      <input placeholder="URL"
+      value={url}
+      onChange={e => setUrl(e.target.value)}></input>
+
+      <input placeholder="Techs"
+      value={techs}
+      onChange={e => setTechs(e.target.value)}></input>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -57,7 +57,7 @@ describe("App component", () => {
 
     await actWait();
 
-    fireEvent.click(getByText("Remover"));
+    fireEvent.click(getByText(`Remover`));
 
     await actWait();
 
